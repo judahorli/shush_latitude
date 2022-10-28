6c02f45ede3a6fc8b6e568a489987baa39d4a183
Judah Perillo <judahorli@gmail.com>
2022-10-28 13:39:29 -0400

    check for time between midnight and 3am

--- shusher.rb
+++ shusher.rb
@@ -8,7 +8,7 @@ bot = Discordrb::Bot.new token: ENV['SHUSH_TOKEN']
 bot.message(author: ENV['LATITUDE']) do |event|
   message = event.message
   msgtime = message.timestamp
-  if msgtime.hour >= 0 and msgtime.hour <= 23
+  if msgtime.hour >= 0 and msgtime.hour <= 3
     event.respond('go to bed latitude', tts = false, embed = nil, attachments = nil, allowed_mentions = nil, message_reference = message)
   end
 end
