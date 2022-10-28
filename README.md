2d06548795228e9bb539d8c5e403b24b3896e62a
Judah Perillo <judahorli@gmail.com>
2022-10-28 13:38:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shusher
+little bot in it for the bit
+currently party popper emoji reacts to any message in all caps, and shush reacts to specific user
---
+++ shusher.rb
@@ -0,0 +1,16 @@
+require "discordrb"
+require "sinatra"
+require "rubygems"
+require "time"
+
+bot = Discordrb::Bot.new token: ENV['SHUSH_TOKEN']
+
+bot.message(author: ENV['LATITUDE']) do |event|
+  message = event.message
+  msgtime = message.timestamp
+  if msgtime.hour >= 0 and msgtime.hour <= 23
+    event.respond('go to bed latitude', tts = false, embed = nil, attachments = nil, allowed_mentions = nil, message_reference = message)
+  end
+end
+
+bot.run
